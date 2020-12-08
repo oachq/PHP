80fd679733bfec1c5a62e8d87e1ed1272dbf630c
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2020-12-08 13:37:34 -0700

    video 10

---
+++ video_10.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    Declaracion de constantes. 
+    </h1>
+    <h2>
+    Video 10
+    </h2>
+<br>
+    <?php
+// para definir una contante tiene que ser en mañuscula y enseguida el valor de la contaste, y enseguida colocar true si deseas manipularla 
+// mandandola a llamar con su nombre en minuscula o mayusculas y agregando una nueva constante con el mismo nombre 
+// y se describre como en el sig ejemplo. 
+
+    define("AUTOR", "oscar", true); // el boolean es para llamar la cste como sea.
+    echo "el autor es: ". autor ;
+    ?>
+</body>
+</html>
