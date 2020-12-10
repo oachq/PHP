321a64663f40cbc63b01d6a879eb59247d663f60
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2020-12-10 13:57:25 -0700

    video 14

---
+++ video_14.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1> Funciones matematicas y casting</h1>
+    <h2>video 14</h2>
+
+    <?php 
+    //se probo algunas funciones aritmeticas comunes.
+    // casting en php se viene por default y para convertir  un string a entero solo hay que indicarlo y viceversa.
+    $num1= rand();
+    echo "el numero es: " .$num1;
+    ?>
+</body>
+</html>
