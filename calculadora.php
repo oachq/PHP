40c73d6e8cdc9a27888e414d88dfe905e078220d
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2020-12-10 13:00:00 -0700

    video 12

---
+++ calculadora.php
@@ -0,0 +1,43 @@
+<style>
+.resul{
+    color:red;
+}
+</style>
+
+<?php
+        if (isset($_POST["button"])){
+
+            $num1=$_POST["num1"];
+            $num2=$_POST["num2"];
+            $operacion=$_POST["operacion"];
+            calcular($operacion);
+        } 
+
+        function calcular ($calculo){ // se crea una funcion para separar las operaciones de la data de entrada
+            if (!strcmp("suma",$calculo)){
+                global $num1;
+                global $num2;
+                $resul= $num1+$num2; // se agrego la variable previamente para poder manipular el css del resultado.. 
+                echo "<p class='resul'> el resultado es: $resul </p>" ;
+            }
+            if (!strcmp("resta",$calculo)){
+                global $num1;
+                global $num2;
+                echo"el resultado es: ". ($num1-$num2);
+            }
+            if (!strcmp("multiplicacion",$calculo)){
+                echo"el resultado es: ". ($num1*$num2);
+            }
+            if (!strcmp("division",$calculo)){
+                global $num1;
+                global $num2;
+                echo"el resultado es: ". ($num1/$num2);
+            }
+            if (!strcmp("modulo",$calculo)){
+                global $num1;
+                global $num2;
+                echo"el resultado es: ". ($num1%$num2);
+            }
+        }
+
+        ?>
---
+++ video_12.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+  
+<h1> 
+Operadores matematicos ||
+</h1>
+        <h3>
+        Video 12.
+        </h3>
+
+      <form name=" form1" method="post" action="calculadora.php">
+        <p>
+        <label for="num1"></label>
+        <input type="text" name="num1" id="num1">
+        <label for="num2"></label>
+        <input type="text" name="num2" id="num2">
+        <label for="operacion"></label>
+        <select name="operacion" id="operacion">
+        <option>suma </option>
+        <option>resta </option>
+        <option>multiplicaicon </option>
+        <option>division </option>
+        <option>modulo </option>
+        </select>
+        </p>
+        <p>
+        <input type="submit" name="button" id="button" value="Enviar" onClick="prueba">
+
+        </p>
+
+      </form>
+</body>
+</html>
