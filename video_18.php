e207a4b85bafc3a6dc651197dda0029fd63db7f8
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2020-12-10 20:27:57 -0700

    video 18

---
+++ video_18.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Bucles while and do while
+    </h1>
+    <h3>
+        video 18.
+    </h3>
+
+    <?php 
+// BUCLE WHILE Y DO WHILE
+
+$var1=7;
+do{
+    echo $var1 . " Estamos ejecutando el codigo del bucle  -do- <br>";
+    $var1++;
+}while ($var1<6 );
+echo " <br>";
+echo " <br>";
+    echo  $var1 . " Estamos ejecutando el codigo del bucle -while- <br>";
+   
+
+
+echo " <br>";
+echo " <br>";
+echo "estamos fuera del bucle";
+
+
+    ?>
+</body>
+</html>
