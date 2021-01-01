74d4d743e8600a218c8a5f461f28765ac2c238ef
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2021-01-01 12:15:09 -0700

    array video 30

---
+++ video_30.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1> ARRAYS </h1>
+    <h2> video 30 </h2>
+
+<?php
+  /* Forma uno de como crear un array en php  
+    $semana[]="lunes";
+    $semana[]="martes";
+    $semana[]="miercoles";
+    echo $semana[1];*/
+
+   /* forma dos de como crear una array en php 
+    $semana=array("lunes","martes","miercoles","jueves","viernes");
+    echo $semana[2];*/
+
+    /* forma tres de como crear un array en php que es por nombre 
+    $datos = array("nombre"=>"oscar","apellido"=>"chavez");
+    echo $datos["apellido"];*/
+
+
+?>
+</body>
+</html>
