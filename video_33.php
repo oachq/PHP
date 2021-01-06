352493e900589742399eff578a007ee03791638e
Oscar Chavez <oscarchavez@MacBook-Pro-de-Oscar.local>
2021-01-05 18:55:23 -0700

    video 33 a 37. definicion base de dato mysql y conexion a database

--- video_33.php
+++ video_33.php
@@ -12,12 +12,44 @@
     </h1>
 
     <h2>
-        video 33
+        video 33-37
     </h2>
 
     <?php
-    
+/*
+    SE REALIZO UNA INTRODUCCION A MYSQUL DE COMO INTERACTUAR CON LA INTERFAS DE MYSQL WEB Y CON LA CONSOLA O TERMINAL.  
+*/
 
+// APARTIR DE AQUI EMPIEZA EL VIDEO 36.
+
+    $db_host="localhost";
+    $db_nombre="prueba";
+    $db_usuario="root";
+    $db_contra="";
+
+    $conexion = mysqli_connect ($db_host, $db_usuario, $db_contra); // mysquli_connect es para mandar las credenciales a la base de datos y conectar
+
+    if (mysqli_connect_errno()){
+        echo "Error 500";// indica que hay error de conexion al server 
+        exit();    
+    }
+
+    mysqli_select_db($conexion,$db_nombre) or die ("no se encontro data base"); //indica el nombre de la db que queremos conectar
+
+    mysqli_set_charset($conexion, "utf8"); // indica que vamos a utilizar los tipos de caracteres español.
+
+    $consulta = "SELECT * FROM datospersonales"; // aqui ingresamos codigo SQL para entrar a la base de datos de "datospersonales"
+
+    $resultados = mysqli_query($conexion, $consulta); // mysqli_query aqui se ejecuta la peticion a ala base de datos, devuelve la data en un array 
+
+   //$fila = mysqli_fetch_row($resultados); // mysqli_fetch_row es para ingresar al array de la data y poder interactuar con el.
+
+    while (( $fila = mysqli_fetch_row($resultados))==true){
+
+        echo $fila[0] . " "; echo $fila[1]. " "; echo $fila[2]. " ";echo $fila[3]. " "; echo "<br>"; // imprimir el  array.
+    }
+  
+    mysqli_close($conexion); // esta funcion es para cerrar la conexion que tienes con una base de datos.
     ?>
 </body>
 </html>
